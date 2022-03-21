91c8a43435ec9406c790df2063f12d0f40e11074
Frank T Guarino <fguarino71@gmail.com>
2022-03-21 17:19:45 -0400

    added load fragment logic in activity main, and updated activity main's layout to include a frame view

--- app/src/main/java/wit/edu/moblieapp/afinal/MainActivity.java
+++ app/src/main/java/wit/edu/moblieapp/afinal/MainActivity.java
@@ -1,6 +1,9 @@
 package wit.edu.moblieapp.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null){
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction transaction = fm.beginTransaction();
+            Fragment video_view = new Video_View();
+            transaction.replace(R.id.container, video_view);
+            transaction.commit();
+        }
+
+        /***
+         * Main Activity may handle the video loading, but fragment, 'video-view.java' should handle only the UI portion
+         *  - Correct me if I'm wrong, just writing what might be how it works or not - Frankie
+         */
+
     }
 }
