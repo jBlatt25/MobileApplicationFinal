7a6223dcabedc93f1e67d1248b0d01414dc6f3c2
Frank T Guarino <fguarino71@gmail.com>
2022-03-24 08:26:14 -0400

    adding small on click func before sqlite implementation

--- app/src/main/java/wit/edu/moblieapp/afinal/loginscreen.java
+++ app/src/main/java/wit/edu/moblieapp/afinal/loginscreen.java
@@ -1,8 +1,14 @@
 package wit.edu.moblieapp.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class loginscreen extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class loginscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginscreen);
+
+
+        Button loginButton = (Button) findViewById(R.id.log_in_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.v("myApp", "BUTTON PRESS");
+                String path = "data/data" + getPackageName() + "/sample.db";
+
+            }
+        });
     }
 }
