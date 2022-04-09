ad43414bcf79a74e42296b9aac80e608cfb05a0a
Frank T Guarino <fguarino71@gmail.com>
2022-04-09 16:35:51 -0400

    open assets forlder for mp4 for vlc, use this branch not frankie/VLCPlayer

--- app/src/main/java/wit/edu/moblieapp/afinal/Video_View.java
+++ app/src/main/java/wit/edu/moblieapp/afinal/Video_View.java
@@ -1,9 +1,13 @@
 package wit.edu.moblieapp.afinal;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.CallSuper;
+import androidx.annotation.MainThread;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +17,7 @@ import org.videolan.libvlc.Media;
 import org.videolan.libvlc.MediaPlayer;
 import org.videolan.libvlc.util.VLCVideoLayout;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -26,27 +31,53 @@ public class Video_View extends Fragment {
 
     private LibVLC mLibVLC = null;
     private MediaPlayer mMediaPlayer = null;
+
+
+    private static String PACKAGE_NAME;
+
+    /***
+     * Main Activity may handle the video loading, but fragment, 'video-view.java' should handle only the UI portion
+     *  - Correct me if I'm wrong, just writing what might be how it works or not - Frankie
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        /***
-         * Main Activity may handle the video loading, but fragment, 'video-view.java' should handle only the UI portion
-         *  - Correct me if I'm wrong, just writing what might be how it works or not - Frankie
-         */
-
-        //final ArrayList<String> args = new ArrayList<>();
-        //args.add("-vvv");
-        //mLibVLC = new LibVLC(inflater.getContext(), args);
-
-
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video__view, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_video__view, container, false);
+
+
+        final ArrayList<String> args = new ArrayList<>();
+        args.add("-vvv");
+        mLibVLC = new LibVLC(inflater.getContext(), args);
+        mMediaPlayer = new MediaPlayer(mLibVLC);
+        mVideoLayout = rootView.findViewById(R.id.video_layout);
+
+
+        PACKAGE_NAME = inflater.getContext().getPackageName();
+
+
+        return rootView;
     }
 
 
+    @MainThread
+    @CallSuper
+    public void onStart() {
 
+        super.onStart();
 
+        mMediaPlayer.attachViews(mVideoLayout, null, ENABLE_SUBTITLES, USE_TEXTURE_VIEW);
 
-
+        //final Media media = new Media(mLibVLC, String.valueOf(getResources().openRawResource(R.raw.boston_split)));
+        final Media media;
+        try {
+            media = new Media(mLibVLC, getContext().getResources().getAssets().openFd("boston_split.mp4"));
+            mMediaPlayer.setMedia(media);
+            media.release();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mMediaPlayer.play();
+    }
 }
